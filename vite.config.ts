3bf8063d2696a8c7c612e4009902e41a23c77096
thnielseen <tess.prog@gmail.com>
2025-04-27 08:21:51 +0200

    🎨 Structure header content with menu and nav and added sections
    - Adds menu with button and main page navigation
    - Adds social link navigation
    - Update prettier configuration
    - Think about a11y perspective
    - Validate HTML and minor corrections

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       '@variables': path.resolve(__dirname, 'src/scss/abstracts/variables'),
       '@functions': path.resolve(__dirname, 'src/scss/abstracts/functions'),
       '@mixins': path.resolve(__dirname, 'src/scss/abstracts/mixins'),
+      '@placeholder': path.resolve(__dirname, 'src/abstracts/placeholders'),
       '@base': path.resolve(__dirname, 'src/scss/base'),
       '@components': path.resolve(__dirname, 'src/scss/components'),
       '@layout': path.resolve(__dirname, 'src/scss/layout'),
