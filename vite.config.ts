86d045a2bc42f17c24c34ab28f7377f2a10e0f27
thnielseen <tess.prog@gmail.com>
2025-04-28 17:05:05 +0200

    🔧 Update Vite and TS config aliases for new SCSS structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@abstracts': path.resolve(__dirname, 'src/scss/abstracts'),
-      '@variables': path.resolve(__dirname, 'src/scss/abstracts/variables'),
-      '@functions': path.resolve(__dirname, 'src/scss/abstracts/functions'),
-      '@mixins': path.resolve(__dirname, 'src/scss/abstracts/mixins'),
-      '@placeholder': path.resolve(__dirname, 'src/abstracts/placeholders'),
-      '@base': path.resolve(__dirname, 'src/scss/base'),
-      '@components': path.resolve(__dirname, 'src/scss/components'),
-      '@layout': path.resolve(__dirname, 'src/scss/layout'),
+      '@abstracts': path.resolve(__dirname, './src/scss/abstracts'),
+      '@variables': path.resolve(__dirname, './src/scss/abstracts/variables'),
+      '@functions': path.resolve(__dirname, './src/scss/abstracts/functions'),
+      '@mixins': path.resolve(__dirname, './src/scss/abstracts/mixins'),
+      '@placeholder': path.resolve(__dirname, './src/scss/abstracts/placeholders'),
+      '@base': path.resolve(__dirname, './src/scss/base'),
+      '@components': path.resolve(__dirname, './src/scss/components'),
+      '@layout': path.resolve(__dirname, './src/scss/layout'),
     },
   },
 });
